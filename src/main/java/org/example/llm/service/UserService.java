7267b62db6509eb596faebbd834902c53536699a
SJ0107 <dhsl357@naver.com>
2024-07-12 16:57:01 +0900

    Merge branch 'main' of https://github.com/Team-InvestGenius/The-First-Step---IntelliJ
    # Conflicts:
    #	src/main/java/org/example/llm/controller/UserController.java

--- src/main/java/org/example/llm/service/UserService.java
+++ src/main/java/org/example/llm/service/UserService.java
@@ -3,18 +3,37 @@ package org.example.llm.service;
 import org.example.llm.Entity.UserEntity;
 import org.example.llm.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Transactional
-    public UserEntity registerUser(UserEntity user) {
-        return userRepository.save(user);
+    public UserEntity registerUser(UserEntity signupRequest) {
+
+
+        // 새 사용자 엔티티 생성
+        UserEntity newUser = UserEntity.builder()
+                .email(signupRequest.getEmail())
+                .password(passwordEncoder.encode(signupRequest.getPassword()))
+                .name(signupRequest.getName())
+                .birthdate(signupRequest.getBirthdate())
+                .investmentType(signupRequest.getInvestmentType())
+                .build();
+
+        // 사용자 저장 및 반환
+        return userRepository.save(newUser);
     }
 
     @Transactional(readOnly = true)
